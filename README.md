380f68325279d3070cb31e95dfb9b642f3a76119
ed0419 <genden80@gmail.com>
2022-01-22 18:48:35 +0800

    finish sending html mail

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## Auto send mail to stu
+
+
+---
+
+### settings.json Format
+> 登入資訊做隱藏
+```json
+{
+    "GMAILTOKEN": "password"
+}
+```
--- send.py
+++ send.py
@@ -8,23 +8,26 @@ import smtplib, json
 with open('settings.json','r',encoding='utf8') as jFile:
     jdata = json.load(jFile)
 
-content = MIMEMultipart()  #建立MIMEMultipart物件
-content["subject"] = "中部資電寒訓資料測試"  #郵件標題
-content["from"] = "s1060015@st.fcjh.tc.edu.tw"  #寄件者
-content["to"] = "genden80@gmail.com" #收件者
-content.attach(MIMEText("Demo python send email"))  #郵件內容
-content.attach(MIMEImage(Path("112314.jpg").read_bytes()))  # 郵件圖片內容
+def sendmail(name,mcemail,mcpass):
+    content = MIMEMultipart()  #建立MIMEMultipart物件
+    content["subject"] = "中部資電寒訓資料測試"  #郵件標題
+    content["from"] = "taichungcomputer@gmail.com"  #寄件者
+    content["to"] = "genden80@gmail.com" #收件者
+    #content.attach(MIMEText("Demo python send email"))  #郵件內容
+    #content.attach(MIMEImage(Path("112314.jpg").read_bytes()))  # 郵件圖片內容
 
-template = Template(Path("success_template.html").read_text())
-body = template.substitute({ "user": "Mike" })
-content.attach(MIMEText(body, "html"))  # HTML郵件內容
+    template = Template(Path("index.htm").read_text())
+    body = template.substitute({ "user": name ,"mcemail":mcemail,"mcpass":mcpass})
+    content.attach(MIMEText(body, "html"))  # HTML郵件內容
 
-with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
-    try:
-        smtp.ehlo()  # 驗證SMTP伺服器
-        smtp.starttls()  # 建立加密傳輸
-        smtp.login("s1060015@st.fcjh.tc.edu.tw", jdata['GMAILTOKEN'])  # 登入寄件者gmail
-        smtp.send_message(content)  # 寄送郵件
-        print("Complete!")
-    except Exception as e:
-        print("Error message: ", e)
+    with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
+        try:
+            smtp.ehlo()  # 驗證SMTP伺服器
+            smtp.starttls()  # 建立加密傳輸
+            smtp.login("taichungcomputer@gmail.com", jdata['GMAILTOKEN'])  # 登入寄件者gmail
+            smtp.send_message(content)  # 寄送郵件
+            print("Complete!")
+        except Exception as e:
+            print("Error message: ", e)
+
+sendmail("HI","iloveu@gmail.com","iiiiiii")
